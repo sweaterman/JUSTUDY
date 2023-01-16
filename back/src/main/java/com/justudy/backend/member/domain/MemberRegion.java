73fed5c2e22c1fdc6cf1211556885f702f2b4e3c
ShinKwang <sklee0206@naver.com>
2023-01-16 15:26:57 +0900

    feat: Add MemberRegion enum
    서울, 대전, 구미, 광주, 부울경을 나타내는 MemberRegion Enum 생성

---
+++ back/src/main/java/com/justudy/backend/member/domain/MemberRegion.java
@@ -0,0 +1,19 @@
+package com.justudy.backend.member.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum MemberRegion {
+
+    SEOUL("서울"),
+    DAEJEON("대전"),
+    GUMI("구미"),
+    GWANGJU("광주"),
+    BUWOOLKYUNG("부울경");
+
+    private final String value;
+
+    MemberRegion(String value) {
+        this.value = value;
+    }
+}
