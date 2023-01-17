6793d2d3533429ea277df566417f24f917538803
이연희 <leeuh7262@naver.com>
2023-01-17 10:53:33 +0900

    S08P12A104-38 feat : 스터디 생성 페이지 수정

--- front/justudy-front/src/router/index.js
+++ front/justudy-front/src/router/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import studyRouter from './studyRouter';
+import userRouter from './userRouter';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [...studyRouter]
+    routes: [...studyRouter, ...userRouter]
 });
 
 export default router;
