896da8f644cd9e511fda3a6acf793519c38b4e2a
quixotic1024 <quixotic1024@gmail.com>
2023-01-17 18:00:47 +0900

    S08P12A104-31 feat: 회원가입 구현

--- front/justudy-front/src/router/index.js
+++ front/justudy-front/src/router/index.js
@@ -4,13 +4,14 @@ import studyRouter from './studyRouter';
 import userRouter from './userRouter';
 import CommunityRouter from './CommunityRouter';
 import selfTestRouter from './selfTestRouter';
+import loginRouter from './loginRouter';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [...studyRouter, ...userRouter, ...CommunityRouter, ...selfTestRouter]
+    routes: [...studyRouter, ...userRouter, ...CommunityRouter, ...selfTestRouter, ...loginRouter]
 });
 
 export default router;
