c01bdbeab4cba903d03b2e5df7be8a05774e17a2
이연희 <leeuh7262@naver.com>
2023-01-18 17:29:45 +0900

    S08P12A104-42 feat : 홍보 중인 스터디 리스트 컴포넌트 구현

---
+++ front/justudy-front/src/views/Study/SearchStudyView.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-app>
+        <!-- 상위 카테고리 tabs -->
+        <v-row>
+            <v-col>
+                <v-tabs v-model="tab" align-with-title>
+                    <v-tabs-slider color="yellow"></v-tabs-slider>
+                    <v-tab v-for="top in categories" :key="top.name">
+                        {{ top.name }}
+                    </v-tab>
+                </v-tabs>
+            </v-col>
+        </v-row>
+
+        <!-- 하위 카테고리 chips -->
+        <v-row>
+            <v-col>
+                <v-chip-group v-model="chip" multiple active-class="primary--text">
+                    <v-chip v-for="bot in categories[Number(tab)].bottom" :key="bot">
+                        {{ bot }}
+                    </v-chip>
+                </v-chip-group>
+            </v-col>
+        </v-row>
+    </v-app>
+</template>
+
+<script>
+// import {mapState} from 'vuex';
+
+export default {
+    name: 'SearchStudyView',
+    data() {
+        return {
+            tab: null,
+            chip: null,
+            categories: [
+                {
+                    name: '프론트엔드',
+                    bottom: ['React', 'vue', 'angular']
+                },
+                {
+                    name: '백엔드',
+                    bottom: ['spring', 'node.js', 'django', 'php']
+                }
+            ]
+        };
+    }
+};
+</script>
