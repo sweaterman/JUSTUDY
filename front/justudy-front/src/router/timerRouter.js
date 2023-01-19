c218e667b7763107f74d27632143a0bdf0c4a100
tjwnrhkd12 <tjwnrhkd12@naver.com>
2023-01-19 17:00:28 +0900

    S08P12A104-41 feat: 실시간 타임체크 구현

---
+++ front/justudy-front/src/router/timerRouter.js
@@ -0,0 +1,21 @@
+import StudyStartView from '@/views/Timer/StudyStart.vue';
+import MyStatisticsView from '@/views/Timer/MyStatistics.vue';
+import FriendStatisticsView from '@/views/Timer/FriendStatistics.vue';
+
+export default [
+    {
+        path: '/timer/study-start',
+        name: 'TimerStudyStart',
+        component: StudyStartView
+    },
+    {
+        path: '/timer/my-statistics',
+        name: 'TimerMyStatistics',
+        component: MyStatisticsView
+    },
+    {
+        path: '/timer/friend-statistics',
+        name: 'TimerFriendStatistics',
+        component: FriendStatisticsView
+    }
+];
---
+++ front/justudy-front/src/views/Timer/FriendStatistics.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-app>rrr </v-app>
+</template>
+<script>
+export default {
+    name: 'FriendStatistics'
+};
+</script>
