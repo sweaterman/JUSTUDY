1da28a27ce245e4577b0b7cb345e8edd05107696
ShinKwang <sklee0206@naver.com>
2023-01-19 16:39:31 +0900

    refactor: Delete Result class
    List를 담을 용도로 만든 Result 클래스를 사용하지 않아 삭제한다.

--- back/src/main/java/com/justudy/backend/common/enum_util/EnumController.java
+++ back/src/main/java/com/justudy/backend/common/enum_util/EnumController.java
@@ -1,6 +1,5 @@
 package com.justudy.backend.common.enum_util;
 
-import com.justudy.backend.common.Result;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
