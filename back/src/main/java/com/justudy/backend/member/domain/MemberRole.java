49c0560748527f7969273a9dbe31683b678c96ae
ShinKwang <sklee0206@naver.com>
2023-01-19 16:31:24 +0900

    S08P12A104-37 refactor: Change name and extend interface

--- back/src/main/java/com/justudy/backend/member/domain/MemberRole.java
+++ back/src/main/java/com/justudy/backend/member/domain/MemberRole.java
@@ -1,6 +1,27 @@
 package com.justudy.backend.member.domain;
 
-public enum MemberRole {
+import com.justudy.backend.common.enum_util.EnumModel;
 
-    USER, MENTO, ADMIN
+public enum MemberRole implements EnumModel {
+
+    USER("회원"),
+    MENTO("멘토"),
+    ADMIN("관리자");
+
+    private final String value;
+
+    MemberRole(String value) {
+        this.value = value;
+    }
+
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
 }
