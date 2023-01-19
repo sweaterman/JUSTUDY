deab972b0e38ac39aa46789f7491a923b4417b08
tjwnrhkd12 <tjwnrhkd12@naver.com>
2023-01-19 13:50:01 +0900

    S08P12A104-41 feat : picture

--- front/justudy-front/src/components/mypage/ListBookMark.vue
+++ front/justudy-front/src/components/mypage/ListBookMark.vue
@@ -1,20 +1,18 @@
 <template>
     <v-container>
         <div v-for="item in 10" v-bind:key="item">
-            <v-row >
-            <v-col cols="2">백엔드</v-col>
-            <v-col cols="7">저 오늘 아파서 결석입니다</v-col>
-            <v-col cols="1">왕싸피</v-col>
-            <v-col cols="2" class="d-flex justify-end">23/01/05</v-col>
-
-        </v-row>
-        <hr>
+            <v-row>
+                <v-col cols="2"><b>백엔드</b></v-col>
+                <v-col cols="7"><b>저 오늘 아파서 결석입니다</b></v-col>
+                <v-col cols="1">왕싸피</v-col>
+                <v-col cols="2" class="d-flex justify-end">23/01/05</v-col>
+            </v-row>
+            <hr />
         </div>
-        
     </v-container>
 </template>
 <script>
-    export default {
-        name : "ListVue",
-    }
+export default {
+    name: 'ListVue'
+};
 </script>
