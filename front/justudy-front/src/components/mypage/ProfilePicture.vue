29d3c911b66543e258264b3bbb475ef6bbaf5691
tjwnrhkd12 <tjwnrhkd12@naver.com>
2023-01-20 15:39:06 +0900

    S08P12A104-35 feat : time

--- front/justudy-front/src/components/mypage/ProfilePicture.vue
+++ front/justudy-front/src/components/mypage/ProfilePicture.vue
@@ -5,18 +5,18 @@
             <Profile :diameter="diameter" :standard="`${standard}`" :src="`${src}`" />
         </div>
         <div class="d-flex">
-            <YellowButton :buttonLength="buttonLength" :height="height" :fontSize="fontSize" :content="`${content}`" :standard="`${standard}`" />
+            <BasicButton :buttonLength="buttonLength" :height="height" :fontSize="fontSize" :content="`${content}`" :standard="`${standard}`" />
             <h1>초보 개발자</h1>
         </div>
     </div>
 </template>
 <script>
-import YellowButton from './YellowButton.vue';
+import BasicButton from '@/components/common/BasicButton.vue';
 import Profile from './Profile.vue';
 export default {
     name: 'FollowFollowing',
     components: {
-        YellowButton,
+        BasicButton,
         Profile
     },
     props: {
--- front/justudy-front/src/views/Timer/MyStatistics.vue
+++ front/justudy-front/src/views/Timer/MyStatistics.vue
@@ -1,23 +1,34 @@
 <template>
-    <v-app>
+    <v-app style="padding-left: 10%; padding-right: 10%">
         <Common />
         <Calendar />
         <div style="margin-bottom: 100px"></div>
         <Boundary content="TOTAL" standard="%" :startPoint="40"></Boundary>
+        <DigitalClockPerDate content="THIS WEEK" />
+        <DigitalClockPerDate content="THIS MONTH" />
         <Boundary content="AVERAGE" standard="%" :startPoint="40"></Boundary>
+        <DigitalClockAverage content="THIS WEEK" :studyTime="15" />
+        <DigitalClockAverage content="THIS MONTH" :studyTime="-15" />
         <Boundary content="SUBJECT" standard="%" :startPoint="40"></Boundary>
+        <TimeAndRadarChart />
     </v-app>
 </template>
 <script>
 import Calendar from '@/components/common/Calendar.vue';
 import Common from '@/components/timer/Common.vue';
-import Boundary from '@/components/mypage/Boundary.vue';
+import Boundary from '@/components/common/Boundary.vue';
+import TimeAndRadarChart from '@/components/timer/TimeAndRadarChart.vue';
+import DigitalClockPerDate from '@/components/timer/DigitalClockPerDate.vue';
+import DigitalClockAverage from '@/components/timer/DigitalClockAverage.vue';
 export default {
     name: 'MyStatistics',
     components: {
         Common,
         Calendar,
-        Boundary
+        Boundary,
+        TimeAndRadarChart,
+        DigitalClockPerDate,
+        DigitalClockAverage
     }
 };
 </script>
