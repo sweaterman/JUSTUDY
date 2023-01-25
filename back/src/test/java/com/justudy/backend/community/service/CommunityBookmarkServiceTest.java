a8567f0f4af03af8db8cdd941b225819e299727e
ShinKwang <sklee0206@naver.com>
2023-01-25 17:04:39 +0900

    refactor: Change import

--- back/src/test/java/com/justudy/backend/community/service/CommunityBookmarkServiceTest.java
+++ back/src/test/java/com/justudy/backend/community/service/CommunityBookmarkServiceTest.java
@@ -7,7 +7,6 @@ import com.justudy.backend.community.dto.response.CommunityResponse;
 import com.justudy.backend.community.repository.CommunityBookmarkRepository;
 import com.justudy.backend.community.repository.CommunityRepository;
 import com.justudy.backend.member.domain.MemberEntity;
-import com.justudy.backend.member.domain.MemberRegion;
 import com.justudy.backend.member.dto.request.MemberCreate;
 import com.justudy.backend.member.repository.MemberRepository;
 import com.justudy.backend.member.service.MemberService;
