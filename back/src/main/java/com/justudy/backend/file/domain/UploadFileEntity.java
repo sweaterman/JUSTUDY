f53a2956e646d6ca6ff400a53f2f11c471b226a0
ShinKwang <sklee0206@naver.com>
2023-01-26 11:22:08 +0900

    feat: Make uploadFile feature

---
+++ back/src/main/java/com/justudy/backend/file/domain/UploadFileEntity.java
@@ -0,0 +1,30 @@
+package com.justudy.backend.file.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "upload_file")
+@Entity
+public class UploadFileEntity {
+
+    @Id @GeneratedValue
+    private Long sequence;
+
+    @Column(name = "upload_filename")
+    private String uploadFileName; //고객이 업로드한 파일명
+
+    @Column(name = "store_filename")
+    private String storeFileName; //서버 내부에서 관리하는 파일명
+
+    @Builder
+    public UploadFileEntity(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
+}
