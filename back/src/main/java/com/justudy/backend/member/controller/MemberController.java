eb04a46751ce23f9b7d2a085c7ebb070caa9dc05
ShinKwang <sklee0206@naver.com>
2023-01-26 10:19:33 +0900

    docs: Add comment for controller api

--- back/src/main/java/com/justudy/backend/member/controller/MemberController.java
+++ back/src/main/java/com/justudy/backend/member/controller/MemberController.java
@@ -23,6 +23,11 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /**
+     * 회원등록 API
+     *
+     * @param request 멤버 가입 요청 객체
+     */
     @PostMapping("/register")
     public ResponseEntity<Void> signupMember(@RequestBody @Validated MemberCreate request) {
         memberService.saveMember(request);
@@ -32,6 +37,7 @@ public class MemberController {
 
     /**
      * 마이페이지 멤버 정보 API
+     *
      * @param session session에서 memberSequence를 찾기 위해
      * @return MypageRespoonse 마이페이지 멤버 응답 객체
      */
@@ -42,6 +48,12 @@ public class MemberController {
         return memberService.getMypage(loginSequence);
     }
 
+    /**
+     * 회원 정보 수정 페이지 API
+     *
+     * @param session session에서 memberSequence를 찾기 위해
+     * @return ModifyPageResonse 수정페이지 멤버 응답 객체
+     */
     @GetMapping("/mypage/modify")
     public ModifyPageResponse getModifyPageInformation(HttpSession session) {
         Long loginSequence = (Long) session.getAttribute(SessionConst.LOGIN_USER);
@@ -49,6 +61,12 @@ public class MemberController {
         return memberService.getModifyPage(loginSequence);
     }
 
+    /**
+     * 회원정보 수정 API
+     *
+     * @param request 수정 요청 정보
+     * @param session 세션에서 sequence 확인
+     */
     @PatchMapping("/mypage/modify")
     public ResponseEntity<Void> modifyMember(@RequestBody @Validated MemberEdit request, HttpSession session) {
         Long loginSequence = (Long) session.getAttribute(SessionConst.LOGIN_USER);
@@ -57,11 +75,22 @@ public class MemberController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * 프로필 조회 API
+     * @param memberSequence
+     * @return ProfileResponse 프로필 응답 객체
+     */
     @GetMapping("/profiles/{memberSequence}")
     public ProfileResponse getProfile(@PathVariable Long memberSequence) {
         return memberService.getProfile(memberSequence);
     }
 
+    /**
+     * 회원 탈퇴 API
+     *
+     * @param session - memberSequence 확인용
+     * 논리적 삭제
+     */
     @DeleteMapping("/mypage/delete")
     public ResponseEntity<Void> quitMember(HttpSession session) {
         Long loginSequence = (Long) session.getAttribute(SessionConst.LOGIN_USER);
@@ -72,10 +101,10 @@ public class MemberController {
 
 
     /**
-     * ADMIN 유저의 회원 삭제 API
+     * ADMIN의 회원 밴 API
      * @param memberSequence - Target Member Sequence
      * @param session
-     *
+     * 논리적 삭제
      */
     @DeleteMapping("/admin/members/{memberSequence}")
     public ResponseEntity<Void> banMember(@PathVariable Long memberSequence, HttpSession session) {
