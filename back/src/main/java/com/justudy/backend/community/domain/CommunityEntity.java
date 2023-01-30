e61607539af638df054c60d7d4ccc5497841553c
ShinKwang <sklee0206@naver.com>
2023-01-30 15:49:08 +0900

    feat: Add category in field

--- back/src/main/java/com/justudy/backend/community/domain/CommunityEntity.java
+++ back/src/main/java/com/justudy/backend/community/domain/CommunityEntity.java
@@ -1,15 +1,16 @@
 package com.justudy.backend.community.domain;
 
+import com.justudy.backend.category.domain.CategoryEntity;
 import com.justudy.backend.member.domain.MemberEntity;
-import com.justudy.backend.member.domain.QMemberEntity;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@AllArgsConstructor
 @Getter
 @Table(name = "community")
 @Entity
@@ -18,32 +19,60 @@ public class CommunityEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "community_seq")
     private Long sequence;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_seq")
     private MemberEntity member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_seq")
-    private Long category_seq;
+    private CategoryEntity category;
+
     @Column(name = "community_title")
     private String title;
+
     @Column(name = "community_content")
     private String content;
+
     @Column(name = "community_view_count")
     private Integer viewCount;
+
     @Column(name = "community_created_time")
     private LocalDateTime createdTime;
+
     @Column(name = "community_modified_time")
     private LocalDateTime modifiedTime;
+
     @Column(name = "community_is_deleted", columnDefinition = "TINYINT(1)")
     private Boolean isDeleted;
+
     @Column(name = "community_is_highlighted", columnDefinition = "TINYINT(1)")
     private Boolean isHighlighted;
+
     @Column(name = "community_week_love_count")
     private Integer weekLoveCount;
+
+    @Builder
+    public CommunityEntity(String title, String content,
+                           Boolean isHighlighted) {
+        this.title = title;
+        this.content = content;
+        this.isHighlighted = isHighlighted;
+        this.viewCount = 0;
+        this.weekLoveCount = 0;
+        this.createdTime = LocalDateTime.now();
+        this.modifiedTime = createdTime;
+        this.isDeleted = false;
+    }
 
     //== 연관관계 편의메소드 ==//
     public void addMember(MemberEntity member) {
         this.member = member;
         //todo Member와 연관관계 업데이트
+    }
+
+    public void changeCategory(CategoryEntity category) {
+        this.category = category;
     }
 
     public void changeViewCount(Integer viewCount) {
