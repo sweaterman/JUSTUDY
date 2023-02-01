3fcb6aafee6ee2490a90cbbeabebb3b979d449ce
ShinKwang <sklee0206@naver.com>
2023-02-01 10:14:24 +0900

    refactor: Change name and code style

--- back/src/main/java/com/justudy/backend/community/service/CommunityBookmarkService.java
+++ back/src/main/java/com/justudy/backend/community/service/CommunityBookmarkService.java
@@ -1,12 +1,14 @@
 package com.justudy.backend.community.service;
 
 import com.justudy.backend.community.domain.CommunityBookmarkEntity;
-import com.justudy.backend.community.exception.InvalidRequest;
 import com.justudy.backend.community.repository.CommunityBookmarkRepository;
+import com.justudy.backend.exception.InvalidRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 @Transactional(readOnly = true)
@@ -15,25 +17,23 @@ public class CommunityBookmarkService {
 
     @Transactional
     public Long createBookmark(Long loginSequence, Long communitySequence) {
-        CommunityBookmarkEntity savedBookmark = bookmarkRepository.readBookmark(loginSequence, communitySequence)
-                .orElseGet(() -> {
-                    CommunityBookmarkEntity newBookmark = createNewBookmark(loginSequence, communitySequence);
-                    bookmarkRepository.save(newBookmark);
-                    return newBookmark;
-                });
+        CommunityBookmarkEntity savedBookmark = bookmarkRepository.findBookmark(loginSequence, communitySequence)
+                .orElseGet(() ->
+                    bookmarkRepository.save(makeNewBookmark(loginSequence, communitySequence))
+                );
 
         return savedBookmark.getSequence();
     }
 
     @Transactional
     public void deleteBookmark(Long loginSequence, Long communitySequence) {
-        CommunityBookmarkEntity findBookmark = bookmarkRepository.readBookmark(loginSequence, communitySequence)
+        CommunityBookmarkEntity findBookmark = bookmarkRepository.findBookmark(loginSequence, communitySequence)
                 .orElseThrow(() -> new InvalidRequest());
 
         bookmarkRepository.delete(findBookmark);
     }
 
-    private CommunityBookmarkEntity createNewBookmark(Long loginSequence, Long communitySequence) {
+    private CommunityBookmarkEntity makeNewBookmark(Long loginSequence, Long communitySequence) {
         return new CommunityBookmarkEntity(loginSequence, communitySequence);
     }
 
