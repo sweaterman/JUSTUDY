3dd2d7bee11a5b767b28d6865743070ca0eafbad
ShinKwang <sklee0206@naver.com>
2023-02-07 11:00:31 +0900

    build: exclude SecurityAutoConfiguration in SpringBootApplication

--- back/src/main/java/com/justudy/backend/BackendApplication.java
+++ back/src/main/java/com/justudy/backend/BackendApplication.java
@@ -2,10 +2,11 @@ package com.justudy.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
