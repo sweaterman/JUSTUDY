ec9054b27b47cc5191633559c1f960694a376575
이연희 <leeuh7262@naver.com>
2023-02-09 17:39:32 +0900

    S08P12A104-203 feat: mm인증 추가 및 스터디 이미지 관련 수정

--- front/justudy-front/src/store/moduleSignUp.js
+++ front/justudy-front/src/store/moduleSignUp.js
@@ -6,7 +6,8 @@ export default {
     state: {
         checkId: false,
         checkNickname: false,
-        checkSsafyId: false
+        checkSsafyId: false,
+        checkMM: false
     },
     getters: {},
     mutations: {
@@ -18,12 +19,14 @@ export default {
         },
         CHECK_SSAFYID(state, payload) {
             state.checkSsafyId = payload;
+        },
+        CHECK_MM(state, payload) {
+            state.checkMM = payload;
         }
     },
     actions: {
         async signUp(_, user) {
             const API_URL = `${port}member/register`;
-            console.log(user);
             await axios({
                 url: API_URL,
                 method: 'POST',
@@ -34,45 +37,65 @@ export default {
                     window.location.href = '/';
                 })
                 .catch(err => {
-                    console.log(err);
+                    if (err.response.status == 409) {
+                        alert('중복된 값이 존재합니다.');
+                    } else if (err.response.status == 401) {
+                        alert('유효하지 않은 MM 정보입니다.');
+                    } else {
+                        console.log(err);
+                    }
                 });
         },
-        async checkVal({commit}, type, content) {
+        async checkVal({commit}, value) {
             let API_URL = `${port}member/check/?`;
-            if (type == 'userid') {
-                API_URL += `userid=${content}`;
-            } else if (type == 'nickname') {
-                API_URL += `nickname=${content}`;
+            if (value.type == 'userId') {
+                API_URL += `userid=${value.content}`;
+            } else if (value.type == 'nickname') {
+                API_URL += `nickname=${value.content}`;
             } else {
-                API_URL += `ssafyId=${content}`;
+                API_URL += `ssafyid=${value.content}`;
             }
             await axios({
                 url: API_URL,
                 method: 'GET'
             })
                 .then(() => {
-                    if (type == 'userid') {
+                    if (value.type == 'userId') {
                         commit('CHECK_ID', true);
-                        alert('성공');
-                    } else if (type == 'nickname') {
+                    } else if (value.type == 'nickname') {
                         commit('CHECK_NICKNAME', true);
                     } else {
                         commit('CHECK_SSAFYID', true);
                     }
+                    alert('사용할 수 있습니다!');
                 })
                 .catch(err => {
                     if (err.response.status == 409) {
-                        if (type == 'userid') {
+                        if (value.type == 'userId') {
                             commit('CHECK_ID', false);
-                            alert('실패');
-                        } else if (type == 'nickname') {
+                        } else if (value.type == 'nickname') {
                             commit('CHECK_NICKNAME', false);
                         } else {
                             commit('CHECK_SSAFYID', false);
                         }
+                        alert('이미 사용하고 있는 값입니다!');
                     } else {
                         console.log(err);
                     }
+                });
+        },
+        async sendMMAPI({commit}, user) {
+            const API_URL = `${port}member/matter-most`;
+            await axios({
+                url: API_URL,
+                method: 'POST',
+                data: user
+            })
+                .then(() => {
+                    commit('CHECK_MM', true);
+                })
+                .catch(err => {
+                    console.log(err);
                 });
         }
     }
