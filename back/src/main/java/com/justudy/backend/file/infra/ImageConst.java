c4f41c22c48f50143e22dbe288b4b9f7d213902a
ShinKwang <sklee0206@naver.com>
2023-02-10 09:44:30 +0900

    refactor: Change dummy data sequence

--- back/src/main/java/com/justudy/backend/file/infra/ImageConst.java
+++ back/src/main/java/com/justudy/backend/file/infra/ImageConst.java
@@ -2,9 +2,9 @@ package com.justudy.backend.file.infra;
 
 public interface ImageConst {
 
-    Long BASIC_MEMBER_IMAGE = 46L;
-    Long BASIC_FRONTEND_IMAGE = 47L;
-    Long BASIC_BACKEND_IMAGE = 48L;
+    Long BASIC_MEMBER_IMAGE = 1L;
+    Long BASIC_FRONTEND_IMAGE = 2L;
+    Long BASIC_BACKEND_IMAGE = 3L;
     Long BASIC_ALGORITHM_IMAGE = 49L;
     Long BASIC_MOBILE_IMAGE = 4L;
     Long BASIC_COMPUTER_SCIENCE_IMAGE = 6L;
