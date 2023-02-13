4503804d86d3c34aeac218f954bb8f5a47b8b91e
ShinKwang <sklee0206@naver.com>
2023-02-13 16:19:39 +0900

    refactor: Separate Validation class from member service

---
+++ back/src/main/java/com/justudy/backend/common/validate/Validation.java
@@ -0,0 +1,16 @@
+package com.justudy.backend.common.validate;
+
+import com.justudy.backend.exception.ForbiddenRequest;
+import com.justudy.backend.member.domain.MemberEntity;
+import com.justudy.backend.member.domain.MemberRole;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Validation {
+
+    public static void validateUserRole(MemberEntity member, MemberRole role) {
+        if (member.getRole().equals(role)) {
+            throw new ForbiddenRequest();
+        }
+    }
+}
