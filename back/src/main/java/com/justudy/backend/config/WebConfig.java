418742098bf88c999cdca87c5392bc3e7618b599
류한민 <ryuhm48@naver.com>
2023-02-14 10:08:16 +0900

    hotfix : cors fix

--- back/src/main/java/com/justudy/backend/config/WebConfig.java
+++ back/src/main/java/com/justudy/backend/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
